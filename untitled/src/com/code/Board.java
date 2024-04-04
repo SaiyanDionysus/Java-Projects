6013e35c3467a7916daca02d0e0a500bccb271e4
SaiyanDionysus <pusika97@gmail.com>
2024-04-04 23:08:05 +0300

    game java

---
+++ untitled/src/com/code/Board.java
@@ -0,0 +1,26 @@
+package com.code;
+
+
+
+
+public class Board extends JPanel implements ActionListener {
+    private final int B_WIDTH = 300;
+    private final int B_HEIGHT = 300;
+    private final int DOT_SIZE = 10;
+    private final int ALL_DOTS = 900;
+    private final int RAND_POS = 29;
+    private final int DELAY = 140;
+
+    private final int x[] = new int[ALL_DOTS];
+    private final int y[] = new int[ALL_DOTS];
+
+    private int dots;
+    private int apple_x;
+    private int apple_y;
+
+    private boolean leftDirection = false;
+    private boolean rightDirection = true;
+    private boolean upDirection = false;
+    private boolean downDirection = false;
+    private boolean inGame = true;
+}
