953561306fcaf5d7ef96249360b3586009929e2b
SaiyanDionysus <pusika97@gmail.com>
2024-04-28 00:30:13 +0300

    end

--- untitled/src/main/java/Snake.java
+++ untitled/src/main/java/Snake.java
@@ -1,5 +1,26 @@
 import javax.swing.*;
+import java.awt.EventQueue;
 
 public class Snake extends JFrame {
+    public Snake() {
+        initUI();
+    }
 
+    private void initUI() {
+        add(new Board());
+
+        setResizable(false);
+        pack();
+
+        setTitle("Snake");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            JFrame ex = new Snake();
+            ex.setVisible(true);
+        });
+    }
 }
