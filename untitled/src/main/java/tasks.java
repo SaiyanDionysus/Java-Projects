5fa59a79d3378ca52b7acf30b1821ba89d6ca1e6
SaiyanDionysus <pusika97@gmail.com>
2024-04-28 23:57:21 +0300

    commit

--- untitled/src/main/java/tasks.java
+++ untitled/src/main/java/tasks.java
@@ -1,13 +1,17 @@
+// Write a Java program to print 'Hello' on screen and your name on a separate line.
+//Expected Output :
+//Hello
+//Alexandra Abramov
+
 import java.util.*;
 
 public class tasks {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String word = scanner.next();
-        if (word.endsWith("ь")) {
-            System.out.println(word.substring(word.length() - 1));
-        } else {
-            System.out.println(word.substring(word.length() - 1));
-        }
+        System.out.print("Введите свое имя: ");
+        String fname = scanner.next();
+        System.out.print("Введите фамилию: ");
+        String lname = scanner.next();
+        System.out.print("Hello \n" + fname + " " + lname);
     }
 }
