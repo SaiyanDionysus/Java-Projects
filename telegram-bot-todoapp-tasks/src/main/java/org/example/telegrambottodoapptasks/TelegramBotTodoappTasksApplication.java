e12b5033ac90cc67970637ca37cbe9e9da7f3827
SaiyanDionysus <pusika97@gmail.com>
2024-09-24 23:15:48 +0300

    bot

---
+++ telegram-bot-todoapp-tasks/src/main/java/org/example/telegrambottodoapptasks/TelegramBotTodoappTasksApplication.java
@@ -0,0 +1,13 @@
+package org.example.telegrambottodoapptasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotTodoappTasksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotTodoappTasksApplication.class, args);
+    }
+
+}
